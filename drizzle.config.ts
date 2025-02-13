b18117c04292cfd7fee672dd7d0489f663958d30
nizarfadlan <hello@nizarfadlan.dev>
2025-02-13 13:03:16 +0700

    initial commit

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { type Config } from "drizzle-kit";
+
+import { env } from "@/env";
+
+export default {
+  schema: "./src/server/db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+  tablesFilter: ["fe_isi_test_nizar_izzuddin_yatim_fadlan_*"],
+} satisfies Config;
